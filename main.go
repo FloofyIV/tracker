d23ef7d8f92a78fdec6e228a67b1e9ea5009422a
FloofyIV <FloofyIV9@proton.me>
2025-11-18 16:08:26 +1300

    full fix

--- main.go
+++ main.go
@@ -10,133 +10,119 @@ import (
 
 type gameData struct {
 	Data []struct {
-		ID                int    `json:"id"`
-		RootPlaceID       int    `json:"rootPlaceId"`
-		Name              string `json:"name"`
-		Description       string `json:"description"`
-		SourceName        any    `json:"sourceName"`
-		SourceDescription any    `json:"sourceDescription"`
-		Creator           struct {
-			ID               int    `json:"id"`
-			Name             string `json:"name"`
-			Type             string `json:"type"`
-			IsRNVAccount     bool   `json:"isRNVAccount"`
-			HasVerifiedBadge bool   `json:"hasVerifiedBadge"`
-		} `json:"creator"`
-		Price                     any       `json:"price"`
-		AllowedGearGenres         []string  `json:"allowedGearGenres"`
-		AllowedGearCategories     []any     `json:"allowedGearCategories"`
-		IsGenreEnforced           bool      `json:"isGenreEnforced"`
-		CopyingAllowed            bool      `json:"copyingAllowed"`
-		Playing                   int       `json:"playing"`
-		Visits                    int64     `json:"visits"`
-		MaxPlayers                int       `json:"maxPlayers"`
-		Created                   time.Time `json:"created"`
-		Updated                   time.Time `json:"updated"`
-		StudioAccessToApisAllowed bool      `json:"studioAccessToApisAllowed"`
-		CreateVipServersAllowed   bool      `json:"createVipServersAllowed"`
-		UniverseAvatarType        string    `json:"universeAvatarType"`
-		Genre                     string    `json:"genre"`
-		GenreL1                   string    `json:"genre_l1"`
-		GenreL2                   string    `json:"genre_l2"`
-		IsAllGenre                bool      `json:"isAllGenre"`
-		IsFavoritedByUser         bool      `json:"isFavoritedByUser"`
-		FavoritedCount            int       `json:"favoritedCount"`
+		ID          int       `json:"id"`
+		Name        string    `json:"name"`
+		Description string    `json:"description"`
+		Updated     time.Time `json:"updated"`
 	} `json:"data"`
 }
 
-var lastUpdate time.Time
-var currentUpdate time.Time
-var name string
-var lastDescription string
-var currentDescription string
 var LogFile *os.File
 
-// var wasDescription bool
-
-func mainLoop(gameID string, webhookURL string, role string, wg *sync.WaitGroup) {
+func mainLoop(gameID, webhookURL, role string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println("Starting update loop.")
-	LogFile.WriteString("Starting update loop.")
+	LogFile.WriteString("Starting update loop.\n")
+
+	var lastUpdate time.Time
+	var lastDescription string
+
 	for {
 		data, err := getUniverseData(gameID)
 		if err != nil {
-			fmt.Println("retrying in 30 seconds, ", err)
+			fmt.Println("retrying in 30 seconds,", err)
 			time.Sleep(30 * time.Second)
 			continue
 		}
-		for _, item := range data.Data { // iterate through every key in the json body, saving them to variables
-			currentUpdate = item.Updated
-			name = item.Name
-			currentDescription = item.Description
-		}
-		if lastDescription == "" {
+
+		item := data.Data[0]
+		currentUpdate := item.Updated
+		currentDescription := item.Description
+		name := item.Name
+
+		if lastUpdate.IsZero() {
+			lastUpdate = currentUpdate
 			lastDescription = currentDescription
-		} else {
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		if currentDescription == lastDescription && currentUpdate.Equal(lastUpdate) {
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		if currentUpdate.After(lastUpdate) {
+
 			if currentDescription != lastDescription {
+
 				fmt.Println("Description updated", time.Now().Format(time.RFC850))
-				fmt.Fprintf(LogFile, "Description updated, %s", time.Now().Format(time.RFC850))
+				fmt.Fprintf(LogFile, "Description updated, %s\n", time.Now().Format(time.RFC850))
+
 				if webhookURL != "" {
+					desc := currentDescription
 					for i := 0; i < 3; i++ {
-						err = webhookSend(name, webhookURL, currentDescription, role)
+						err := webhookSend(name, webhookURL, desc, role)
 						if err == nil {
 							break
-						} else {
-							fmt.Println(err)
 						}
+						fmt.Println(err)
+						time.Sleep(2 * time.Second)
 					}
 				}
+
 				lastDescription = currentDescription
 				lastUpdate = currentUpdate
 				time.Sleep(30 * time.Second)
 				continue
 			}
-		}
-		if lastUpdate.IsZero() {
-			lastUpdate = currentUpdate // if lastUpdate is empty, make it equal to current update
-			fmt.Println("lastUpdate <- currentUpdate")
-		} else {
-			if currentUpdate.After(lastUpdate) { // if the current update time is later than the last update, run this
-				fmt.Println("update detected", time.Now().UTC())
-				if webhookURL != "" {
-					for i := 0; i < 3; i++ {
-						err = webhookSend(name, webhookURL, "", role) // try to send to the webhook 3 times
-						if err == nil {
-							break // break out of the 3 time send loop if it succeeds.
-						} else {
-							fmt.Println(err)
-						}
+
+			fmt.Println("Update detected", time.Now().UTC())
+			if webhookURL != "" {
+				for i := 0; i < 3; i++ {
+					err := webhookSend(name, webhookURL, "", role)
+					if err == nil {
+						break
 					}
+					fmt.Println(err)
+					time.Sleep(2 * time.Second)
 				}
-				lastUpdate = currentUpdate
 			}
+
+			lastUpdate = currentUpdate
+			time.Sleep(30 * time.Second)
+			continue
 		}
+
 		time.Sleep(30 * time.Second)
 	}
 }
 
 func main() {
 	var err error
+
 	webhookURL := os.Getenv("WEBHOOK")
 	placeID := os.Getenv("PLACE")
 	pingRole := os.Getenv("ROLE")
+
 	LogFile, err = os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0655)
 	if err != nil {
-		fmt.Println(LogFile, err)
 		panic(err)
 	}
-	fmt.Fprintf(LogFile, "Tracker started, %s", time.Now().Format(time.RFC850))
-	var wg sync.WaitGroup
+	fmt.Fprintf(LogFile, "Tracker started, %s\n", time.Now().Format(time.RFC850))
 
 	if webhookURL == "" {
-		fmt.Println("running with no webhook, set with PLACE=\"https://discord.com/api/webhook/xxx/xxx\"")
-	} else if placeID == "" {
-		log.Fatal("please set the placeID (PLACE=\"123456789\" ./tracker)")
+		fmt.Println("Running with no webhook.")
 	}
-	fmt.Printf("\033[KGetting universeID\r")
+	if placeID == "" {
+		log.Fatal("Please set PLACE env var")
+	}
+
+	fmt.Printf("Getting universeID...\n")
 	universeID := getUniverseFromPlaceID(placeID)
-	LogFile.WriteString("Got UniverseID")
-	fmt.Printf("\033[KGot universeID\n")
+	fmt.Printf("Got universeID\n")
+	LogFile.WriteString("Got UniverseID\n")
+
+	var wg sync.WaitGroup
 	wg.Add(1)
 	go mainLoop(universeID, webhookURL, pingRole, &wg)
 	wg.Wait()
--- utils.go
+++ utils.go
@@ -14,11 +14,13 @@ import (
 func getUniverseFromPlaceID(PlaceID string) string {
 	var universeID string
 	var fails int
+
 	for {
 		if fails >= 5 {
-			fmt.Printf("Too many fails, trying again in 15 minutes. (%d fails)", fails)
+			fmt.Printf("Too many fails, trying again in 15 minutes. (%d fails)\n", fails)
 			time.Sleep(15 * time.Minute)
 		}
+
 		client := &http.Client{}
 		url := "https://apis.roblox.com/universes/v1/places/" + PlaceID + "/universe"
 
@@ -29,12 +31,12 @@ func getUniverseFromPlaceID(PlaceID string) string {
 			time.Sleep(30 * time.Second)
 			continue
 		}
-		req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0")
+		req.Header.Set("User-Agent", "Mozilla/5.0")
 
 		resp, err := client.Do(req)
 		if err != nil {
 			fails++
-			fmt.Println("Failed to get/send response/request, retrying in 30 seconds.")
+			fmt.Println("Failed to get response, retrying in 30 seconds.")
 			time.Sleep(30 * time.Second)
 			continue
 		}
@@ -43,25 +45,28 @@ func getUniverseFromPlaceID(PlaceID string) string {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			fails++
-			fmt.Println("Failed to read the body, retrying in 30 seconds")
+			fmt.Println("Failed to read body, retrying in 30 seconds.")
 			time.Sleep(30 * time.Second)
 			continue
 		}
-		universeID = gjson.Get(string(body), "universeId").String()
 
+		universeID = gjson.GetBytes(body, "universeId").String()
 		if universeID == "" {
 			fails++
-			fmt.Println("Failed to get universe id, retrying in 30 seconds.")
+			fmt.Println("Failed to get universe ID, retrying in 30 seconds.")
 			time.Sleep(30 * time.Second)
 			continue
 		}
+
 		break
 	}
+
 	return universeID
 }
 
-func webhookSend(name string, webhookURL string, description string, role string) error {
+func webhookSend(name, webhookURL, description, role string) error {
 	var embed discordwebhook.Embed
+
 	if description == "" {
 		embed = discordwebhook.Embed{
 			Title:     name,
@@ -71,11 +76,8 @@ func webhookSend(name string, webhookURL string, description string, role string
 				Name:     "Aesthetical",
 				Icon_URL: "https://cdn.discordapp.com/avatars/1419099472650043555/c11c5e3a7e55d7adc756f47a956eb6fb.webp?size=1024",
 			},
-
 			Fields: []discordwebhook.Field{
-				{
-					Value: "Update detected.",
-				},
+				{Value: "Update detected."},
 			},
 		}
 	} else {
@@ -87,21 +89,20 @@ func webhookSend(name string, webhookURL string, description string, role string
 				Name:     "Aesthetical",
 				Icon_URL: "https://cdn.discordapp.com/avatars/1419099472650043555/c11c5e3a7e55d7adc756f47a956eb6fb.webp?size=1024",
 			},
-
 			Fields: []discordwebhook.Field{
 				{
 					Name:  "Description updated",
-					Value: currentDescription,
+					Value: description,
 				},
 			},
 		}
 	}
+
 	var hook discordwebhook.Hook
 	if role == "" {
 		hook = discordwebhook.Hook{
 			Username:   "Aesthetical",
 			Avatar_url: "https://cdn.discordapp.com/avatars/1419099472650043555/c11c5e3a7e55d7adc756f47a956eb6fb.webp?size=1024",
-			Content:    "",
 			Embeds:     []discordwebhook.Embed{embed},
 		}
 	} else {
@@ -118,33 +119,23 @@ func webhookSend(name string, webhookURL string, description string, role string
 		return err
 	}
 
-	err = discordwebhook.ExecuteWebhook(webhookURL, payload)
-	if err != nil {
-		return err
-	}
-	return err
+	return discordwebhook.ExecuteWebhook(webhookURL, payload)
 }
 
 func getUniverseData(gameID string) (gameData, error) {
-	url := "https://games.roblox.com/v1/games?universeIds=" + gameID // game url
-	fmt.Printf("Sending request...\r")
-	resp, err := http.Get(url) // http.Get() the game url -> resp
+	url := "https://games.roblox.com/v1/games?universeIds=" + gameID
+	resp, err := http.Get(url)
 	if err != nil {
 		return gameData{}, err
 	}
-	fmt.Printf("\033[KRecieved data, %d\n", resp.StatusCode)
+	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body) // extract the body from the response
-	resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return gameData{}, err
 	}
 
 	var game gameData
-	err = json.Unmarshal(body, &game) // extract the json data from the body -> game
-	if err != nil {
-		return gameData{}, err
-	}
-
+	err = json.Unmarshal(body, &game)
 	return game, err
 }
