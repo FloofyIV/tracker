436de94ac1a0f13fe0ccdbd30e6998a1df929317
FloofyIV <FloofyIV9@proton.me>
2025-11-18 14:55:36 +1300

    fixed logic bug, working on implementing a log file.

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -11,29 +12,50 @@ import (
 )
 
 func getUniverseFromPlaceID(PlaceID string) string {
-	client := &http.Client{}
-	url := "https://apis.roblox.com/universes/v1/places/" + PlaceID + "/universe"
+	var universeID string
+	var fails int
+	for {
+		if fails >= 5 {
+			fmt.Printf("Too many fails, trying again in 15 minutes. (%d fails)", fails)
+			time.Sleep(15 * time.Minute)
+		}
+		client := &http.Client{}
+		url := "https://apis.roblox.com/universes/v1/places/" + PlaceID + "/universe"
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0")
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			fails++
+			fmt.Println("Failed to create new GET request, retrying in 30 seconds.")
+			time.Sleep(30 * time.Second)
+			continue
+		}
+		req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0")
 
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
+		resp, err := client.Do(req)
+		if err != nil {
+			fails++
+			fmt.Println("Failed to get/send response/request, retrying in 30 seconds.")
+			time.Sleep(30 * time.Second)
+			continue
+		}
+		defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	universeID := gjson.Get(string(body), "universeId").String()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fails++
+			fmt.Println("Failed to read the body, retrying in 30 seconds")
+			time.Sleep(30 * time.Second)
+			continue
+		}
+		universeID = gjson.Get(string(body), "universeId").String()
 
-	if universeID == "" {
-		panic("failed to get universeID, quitting.")
+		if universeID == "" {
+			fails++
+			fmt.Println("Failed to get universe id, retrying in 30 seconds.")
+			time.Sleep(30 * time.Second)
+			continue
+		}
+		break
 	}
 	return universeID
 }
@@ -101,4 +123,28 @@ func webhookSend(name string, webhookURL string, description string, role string
 		return err
 	}
 	return err
+}
+
+func getUniverseData(gameID string) (gameData, error) {
+	url := "https://games.roblox.com/v1/games?universeIds=" + gameID // game url
+	fmt.Printf("Sending request...\r")
+	resp, err := http.Get(url) // http.Get() the game url -> resp
+	if err != nil {
+		return gameData{}, err
+	}
+	fmt.Printf("\033[KRecieved data, %d\n", resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body) // extract the body from the response
+	resp.Body.Close()
+	if err != nil {
+		return gameData{}, err
+	}
+
+	var game gameData
+	err = json.Unmarshal(body, &game) // extract the json data from the body -> game
+	if err != nil {
+		return gameData{}, err
+	}
+
+	return game, err
 }
